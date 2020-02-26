382f913a4f3330e03ac3871132f7531deba54963
kisonKim <npolly7@gmail.com>
2020-02-26 22:57:56 +0900

    4th commit

--- 06_conversion2/06_conversion2/main.cpp
+++ 06_conversion2/06_conversion2/main.cpp
@@ -7,8 +7,10 @@
 //
 
 #include <stdio.h>
-int card_convr(unsigned x,int n, char d[]){
+#define SWAP(type,x,y) do{type t = x; x=y; y=t;}while(0)
 
+int card_convr2(unsigned x,int n, char d[]){
+    
     char dchar[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     int digits = 0;
     if(x == 0 )
@@ -18,11 +20,18 @@ int card_convr(unsigned x,int n, char d[]){
             d[digits++] = dchar[x%n];
             x/=n;
         }
+    
+    for(int i=0;i<digits/2;i++)
+    {
+        SWAP(char, d[i], d[digits-i-1]);
+    }
+    
     return digits;
     return 0;
 }
 
 //진수 변환 알고리즘
+//05와 다르게 배열의 앞자리에 나눠진 수가 들어가게
 int main(int argc, const char * argv[]) {
     int i;
     unsigned no;
@@ -36,9 +45,9 @@ int main(int argc, const char * argv[]) {
         scanf("%u",&no);
         printf("어떤 진수로 변환할까요?(2-36) : ");
         scanf("%d",&cd);
-        dno = card_convr(no,cd,cno);
+        dno = card_convr2(no,cd,cno);
         printf("%d진수로는 ",cd);
-        for(i=dno-1;i>=0;i--){
+        for(i=0; i<dno; i++){
             printf("%c",cno[i]);
         }
         printf("입니다.\n");
